eab64eed7e88ca49bd840f55b28b9f16886a894c
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-22 21:20:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,7 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash belowend
+end
 
 def adding
   shipping_manifest = {
@@ -23,6 +22,11 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] = "2"
+  shipping_manifest["gun powder"] = "4"
+
+  shipping_manifest
+  
   # add 2 muskets to the shipping_manifest hash below
 
 
