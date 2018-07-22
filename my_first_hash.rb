736ea23fce84325a8287e619e25880808f652c04
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-22 21:21:33 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,14 +27,5 @@ def adding
 
   shipping_manifest
 
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
